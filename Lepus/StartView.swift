a3d01455c31bdfc2d7ab6a34869e713b508c4184
Aw Joey <s10196528@connect.np.edu.sg>
2022-01-15 23:00:02 +0800

    working on start page

--- Lepus/StartView.swift
+++ Lepus/StartView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("startBackground")
+            VStack{
+                Text("Lepus")
+                    .font(Font.custom("Sansita-BoldItalic", size:32))
+                
+            }
+        }
     }
 }
 
