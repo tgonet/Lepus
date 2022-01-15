2ff7835fa91de42cf64c190dde801ea2b4b5197f
Aw Joey <s10196528@connect.np.edu.sg>
2022-01-15 23:30:41 +0800

    StartView UI page complete

--- Lepus/StartView.swift
+++ Lepus/StartView.swift
@@ -12,10 +12,37 @@ struct StartView: View {
         ZStack{
             Image("startBackground")
             VStack{
+                Spacer()
                 Text("Lepus")
                     .font(Font.custom("Sansita-BoldItalic", size:32))
-                
+                    .foregroundColor(Color.white)
+                    .frame(maxWidth: .infinity, alignment:.leading)
+                Text("The grind never ends")
+                    .font(Font.custom("Rubik-Regular", size:18))
+                    .foregroundColor(Color.white)
+                    .padding(.bottom)
+                    .frame(maxWidth: .infinity, alignment:.leading)
+                Button(action:{}, label: {
+                    Text("Login")
+                        .font(Font.custom("Rubik-Regular", size:22))
+                        .foregroundColor(Color.black)
+                })
+                    .padding(20)
+                    .frame(width: 350, height: 60)
+                    .background(Color("AccentColor"))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.bottom)
+                Button(action:{}, label: {
+                    Text("Register")
+                        .font(Font.custom("Rubik-Regular", size:22))
+                        .foregroundColor(Color.white)
+                })
+                    .padding(20)
+                    .frame(width: 350, height: 60)
+                    .background(Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
             }
+            .padding(120)
         }
     }
 }
