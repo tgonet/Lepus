ca94e3be86dba5f04850294102a5c61244c26108
Aw Joey <s10196528@connect.np.edu.sg>
2022-01-19 03:01:17 +0800

    Login - Fix navigation issue, added show password toggler, changed fonts & background

--- Lepus/LoginView.swift
+++ Lepus/LoginView.swift
@@ -11,47 +11,77 @@ import Firebase
 struct LoginView: View {
     @State private var email:String = ""
     @State private var password:String = ""
+    @State private var showPassword = false
     @State private var Redirect = false
+    @State private var authFail = false
     
     var body: some View {
-        ZStack{
-            VStack{
-                Image("runImage")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.top).frame(height: 400)
-                HStack(alignment:.center, spacing:15){
-                    Image(systemName: "envelope")
-                    TextField("Email", text: $email)
+        NavigationView{
+            ZStack{
+                VStack{
+                    Image("runImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.top).frame(height: 400)
+                    HStack(alignment:.center, spacing:10){
+                        Image(systemName: "envelope")
+                        TextField("Email", text: $email)
+                            .autocapitalization(.none)
+                            .font(Font.custom("Rubik-Regular", size:18))
+                            .disableAutocorrection(true)
+                        }
+                        .padding(.vertical,12)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .foregroundColor(Color.black)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
+                        .cornerRadius(10)
+                        .padding()
+                    
+                    HStack(alignment:.center, spacing:15){
+                        Image(systemName: "lock")
+                        if (showPassword)
+                        {
+                            TextField("Password", text: $password)
+                                .autocapitalization(.none)
+                                .font(Font.custom("Rubik-Regular", size:18))
+                                .disableAutocorrection(true)
+                        }
+                        else{
+                            SecureField("Password", text: $password)
+                                .autocapitalization(.none)
+                                .font(Font.custom("Rubik-Regular", size:18))
+                                .disableAutocorrection(true)
+                        }
+                        
+                        Button(action:{
+                            self.showPassword.toggle()
+                        })
+                        {
+                            Image(systemName: self.showPassword == true ? "eye":"eye.slash")
+                                .foregroundColor(Color("AccentColor"))
+                            
+                        }
+                         
                     }
-                    .padding(.vertical,12)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
-                    .cornerRadius(10)
-                    .padding()
-                
-                HStack(alignment:.center, spacing:15){
-                    Image(systemName: "lock")
-                    SecureField("Password", text: $password)
-                    }
-                    .padding(.vertical,12)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
-                    .cornerRadius(10)
-                    .padding()
-                NavigationLink(destination: TabViewUI(), isActive: $Redirect) {
+                        .padding(.vertical,12)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .foregroundColor(Color.black)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
+                        .cornerRadius(10)
+                        .padding()
+                    
                     Button(action:{
-                        print("HI")
                         Login(email: email, password: password)
                     }, label:{
                         HStack{
                             Spacer(minLength: 0)
                             Text("Login")
                                 .foregroundColor(Color.black)
+                                .font(Font.custom("Sansita-BoldItalic", size:20))
                             Spacer(minLength: 0)
                             
                         }
@@ -62,8 +92,20 @@ struct LoginView: View {
                         .padding()
                     })
                         .opacity((email.isEmpty || password.isEmpty) ? 0.8:1)
-                        .disabled((email.isEmpty || password.isEmpty) ? true:false)
+                        .disabled((email.isEmpty || password.isEmpty))
+                
+                    NavigationLink(destination: TabViewUI(), isActive: $Redirect) {
+                        EmptyView()
+                    }
+                    
+                    if authFail{
+                        Text("Authentication failed. Please try again")
+                            .foregroundColor(Color.red)
+                            .font(Font.custom("Rubik-Regular", size:16))
+                    }
                 }
+                .background(Color("BackgroundColor"))
+                
             }
         }
     }
@@ -72,8 +114,10 @@ struct LoginView: View {
         let auth = Auth.auth()
         auth.signIn(withEmail: email, password: password, completion: { result, error in
             guard result != nil, error == nil else{
+                authFail = true
                 return
             }
+            
             let uid = result!.user.uid
             var ref: DatabaseReference!
             ref = Database.database().reference()
@@ -84,33 +128,13 @@ struct LoginView: View {
                 }
                 print("managed to get ref")
                 let value = snapshot.value as? NSDictionary
-                var name:String = value?["name"] as? String ?? ""
-                var email:String = value?["email"] as? String ?? ""
+                let name:String = value?["name"] as? String ?? ""
+                let email:String = value?["email"] as? String ?? ""
                 user = User(email: email, name: name)
+                print("\(user!.name), \(user!.email)")
                 self.Redirect = true
             })
         })
-        
-        
-//        if auth.currentUser != nil
-//        {
-//            let uid = auth.currentUser!.uid
-//            var ref: DatabaseReference!
-//            ref = Database.database().reference()
-//            ref.child("users/\(uid)").getData(completion: {error, snapshot in
-//                guard error == nil else {
-//                    print(error!.localizedDescription)
-//                    return;
-//                }
-//                print("managed to get ref")
-//                user = snapshot.value as? User ?? user
-//            })
-//            
-//            if user != nil{
-//                print("user not nil")
-//                self.Redirect = false
-//            }
-//        }
     }
 }
 
