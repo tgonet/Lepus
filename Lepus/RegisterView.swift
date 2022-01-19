41538cc215df708372710ac1920a3d38b90d3091
Pan Ye Cheng <panyc63@gmail.com>
2022-01-19 16:48:59 +0800

    Register Updated
    tried Progress View - TBC
    updated UI
    Register user info uploaded to database

--- Lepus/RegisterView.swift
+++ Lepus/RegisterView.swift
@@ -15,21 +15,30 @@ struct RegisterView: View {
     @State private var password:String = ""
     @State private var confirmPassword:String = ""
     
+    @State private var showPassword = false
+    @State private var showConfirm = false
     @State private var showAlert = false
+    @State private var isLoading = false
       
-    @State private var ref: DatabaseReference!
-    @State private var auth = Auth.auth()
     
     @State var selection: Int? = nil
     
     var body: some View {
-        VStack{
-            Text("Register")
-                .font(Font.custom("Rubik-Regular", size:30))
-                .foregroundColor(.black)
-                .fontWeight(.heavy)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top,300)
+        ZStack{
+            
+        
+            VStack{
+                ScrollView{
+                Image("runImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    
+                    if isLoading {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .scaleEffect(3)
+                                
+                    }
                 
             HStack(alignment:.center, spacing:15){
                 Image(systemName: "envelope")
@@ -44,7 +53,7 @@ struct RegisterView: View {
             
             HStack(spacing:15){
                 Image(systemName: "person.fill")
-                SecureField("Name",text:$name)
+                TextField("Name",text:$name)
 
             }.padding(.vertical,12)
              .padding(.horizontal)
@@ -53,88 +62,148 @@ struct RegisterView: View {
              .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
              .padding()
              .padding(.top,-15)
-
-
-            
-            HStack(spacing:15){
+             
+            HStack(alignment:.center, spacing:15){
                 Image(systemName: "lock")
-                SecureField("Password", text: $password)
-                }
-                .padding(.vertical,12)
-                .padding(.horizontal)
-                .background(Color.white)
-                .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
-                .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
-                .padding()
-                .padding(.top,-15)
-
-
+                if (showPassword)
+                {
+                        TextField("Password", text: $password)
+                            .autocapitalization(.none)
+                            .font(Font.custom("Rubik-Regular", size:18))
+                            .disableAutocorrection(true)
+                    }
+                    else{
+                        SecureField("Password", text: $password)
+                            .autocapitalization(.none)
+                            .font(Font.custom("Rubik-Regular", size:18))
+                            .disableAutocorrection(true)
+                    }
                     
+                    Button(action:{
+                        self.showPassword.toggle()
+                    })
+                    {
+                        Image(systemName: self.showPassword == true ? "eye":"eye.slash")
+                            .foregroundColor(Color("AccentColor"))
+                        
+                    }
+                     
+                }.padding(.vertical,12)
+                 .padding(.horizontal)
+                 .background(Color.white)
+                 .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                 .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
+                 .padding()
+                 .padding(.top,-15)
+                
+                HStack(alignment:.center, spacing:15){
+                    Image(systemName: "lock")
+                    if (showConfirm)
+                    {
+                        TextField("Confirm Password", text: $confirmPassword)
+                            .autocapitalization(.none)
+                            .font(Font.custom("Rubik-Regular", size:18))
+                            .disableAutocorrection(true)
+                    }
+                    else{
+                        SecureField("Confirm Password", text: $confirmPassword)
+                            .autocapitalization(.none)
+                            .font(Font.custom("Rubik-Regular", size:18))
+                            .disableAutocorrection(true)
+                    }
+                    
+                    Button(action:{
+                        self.showConfirm.toggle()
+                    })
+                    {
+                        Image(systemName: self.showConfirm == true ? "eye":"eye.slash")
+                            .foregroundColor(Color("AccentColor"))
+                        
+                    }
+                     
+                }.padding(.vertical,12)
+                 .padding(.horizontal)
+                 .background(Color.white)
+                 .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
+                 .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
+                 .padding()
+                 .padding(.top,-15)
+                
             
-            HStack(spacing:15){
-                Image(systemName: "lock")
-                TextField("Confirm Password",text:$confirmPassword)
-            }.padding(.vertical,12)
-             .padding(.horizontal)
-             .background(Color.white)
-             .shadow(color: Color.black.opacity(0.05), radius: 5, x: 5, y: 5)
-             .shadow(color: Color.black.opacity(0.05), radius: 5, x: -5, y: -5)
-             .padding()
-             .padding(.top,-15)
-
             Spacer()
             //Redirect to home page? upon creation
             
             NavigationLink(destination: TabViewUI(), tag: 1, selection: $selection) {
                 Button(action:{
+                    hideKeyboard()
                     registerUser(email: email, name: name, password: password, confirmPass: confirmPassword)
                 }, label:{
                     HStack{
                         Spacer(minLength: 0)
                         Text("Register")
+                            .foregroundColor(Color.black)
+                            .font(Font.custom("Sansita-BoldItalic", size:20))
                         Spacer(minLength: 0)
                         
                     }
-                    .foregroundColor(.white)
                     .padding(.vertical,12)
-                    .padding(.horizontal)
-                    .background(.yellow)
+                    .padding(.horizontal,20)
+                    .background(Color("AccentColor"))
                     .cornerRadius(10)
                     .padding()
-                })
-                //Disable if inputs not entered
-                    .opacity((email != "" && name != "" && password != "" && confirmPassword != "") ? 1:0.6)
-                    .disabled((email != "" && name != "" && password != "" && confirmPassword != "") ? false:true)
-                    .alert(isPresented: $showAlert){
-                                        Alert(title: Text("Password does not match"), message: Text("Password and Confirm Password do not match"), dismissButton: .default(Text("Ok")))
+                            })
+                
+                        }
+                    //Disable if inputs not entered
+                        .opacity((email != "" && name != "" && password != "" && confirmPassword != "") ? 1:0.6)
+                        .disabled((email != "" && name != "" && password != "" && confirmPassword != "") ? false:true)
+                        .alert(isPresented: $showAlert){
+                                            Alert(title: Text("Password does not match"), message: Text("Password and Confirm Password do not match"), dismissButton: .default(Text("Ok")))
+                    }
                 }
+                .edgesIgnoringSafeArea(.top)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
             }
-            }.padding(.horizontal)
         }
+    }
 func registerUser(email:String,name:String, password:String,confirmPass:String){
     if(password == confirmPass){
         showAlert = false
-        //ref = Database.database().reference()
-        //let user:User = User(email: email, name: name, password: password)
-       /*
-        let jsonEncoder = JSONEncoder()
-        let jsonData = try! jsonEncoder.encode(user)
-                            
-        let json = String (data:jsonData,encoding:String.Encoding.utf8)
-                        
-        self.ref.child("user1").setValue(json)
+        var ref: DatabaseReference!
+        let auth = Auth.auth()
+        do {
+          try auth.signOut()
+        } catch let err{
+            print(err)
+        }
         
-        */
-        
-        auth.createUser(withEmail: email, password: password){ result, error in guard result != nil, error == nil else {
+        ref = Database.database().reference()
+        auth.createUser(withEmail: email, password: password){ (result, error) in
+            if error == nil {
+
+        } else {
             return
         }}
+        //startLoading()
+        sleep(5)
+        let currentUser = auth.currentUser
+        let myDict:[String: String] = ["Email":email, "Name":name]
+        ref.child("users").child(currentUser!.uid).setValue(myDict)
+
         self.selection = 1
 
         } else if (password != confirmPass){
             showAlert = true
         }
                         
+    }
+
+    
+    func startLoading(){
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now()+5){
+            isLoading = false
+        }
     }
 }
 
