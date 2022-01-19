07ad035c7e8f2e1cfc1bb36d5f16a9b04d3ddbae
Ming Zhe <tanmingzhe.99@gmail.com>
2022-01-19 21:46:01 +0800

    Able to save map screenshot to firebase with retrievable url

--- Lepus/RunView.swift
+++ Lepus/RunView.swift
@@ -13,146 +13,145 @@ import FirebaseStorage
 struct RunView: View {
     
     @Environment(\.presentationMode) var presentationMode
-    
     @State private var showingAlert = false
-    
     @ObservedObject var stopwatchManager = StopwatchManager()
-    
     var span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
     @State private var snapshotImage: UIImage? = nil
     
     var body: some View {
-        VStack{
-            MapView(region: MKCoordinateRegion(center: stopwatchManager.locationManager.location.coordinate, span: span),lineCoordinates: stopwatchManager.lineCoordinates).ignoresSafeArea(edges: .top).onAppear{
-                stopwatchManager.start()
-                print("initss")
-            }
+        ZStack {
             VStack{
-                Text("Duration")
-                    .font(Font.custom("Rubik-Regular", size:12))
-                Text(stopwatchManager.timeStr)
-                    .font(Font.custom("Sansita-BoldItalic", size:60))
-            }
-            HStack(spacing:50){
-                VStack{
-                    Text("Distance")
-                        .font(Font.custom("Rubik-Regular", size:12))
-                    Text("\(String(format: "%.2f", stopwatchManager.distance))km")
-                        .font(Font.custom("Sansita-BoldItalic", size:32))
+                MapView(region: MKCoordinateRegion(center: stopwatchManager.locationManager.location.coordinate, span: span),lineCoordinates: stopwatchManager.lineCoordinates).ignoresSafeArea(edges: .top).onAppear{
+                    stopwatchManager.start()
+                    print("initss")
                 }
+                
                 VStack{
-                    Text("Avg Pace")
+                    Text("Duration")
                         .font(Font.custom("Rubik-Regular", size:12))
-                    Text("\(String(format: "%.2f", stopwatchManager.avePace))\"")
-                        .font(Font.custom("Sansita-BoldItalic", size:32))
+                    Text(stopwatchManager.timeStr)
+                        .font(Font.custom("Sansita-BoldItalic", size:60))
                 }
-            }.padding(10)
-        
-            if stopwatchManager.mode == .running{
-                HStack(spacing: 55){
-                    Button(action:{self.stopwatchManager.pause()}, label: {
-                        Image("Logo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width:70)
-                            .padding(.trailing, 8)
-                        Text("Lepus OFF")
+                HStack(spacing:50){
+                    VStack{
+                        Text("Distance")
+                            .font(Font.custom("Rubik-Regular", size:12))
+                        Text("\(String(format: "%.2f", stopwatchManager.distance))km")
                             .font(Font.custom("Sansita-BoldItalic", size:32))
-                            .foregroundColor(Color.black)
-                    })
-                        .padding(20)
-                        .frame(width: 350, height: 80)
-                        .background(Color("AccentColor3"))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .shadow(color: .gray, radius: 4, x: 5, y: 5)
-                }.padding(30)
-            }
-            else{
-                HStack(spacing: 55){
-                    Button(action:{self.stopwatchManager.start()}, label: {
-                        Image(systemName: "play.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("AccentColor2"))
-                            .padding(8)
-                    })
-                        .frame(width: 80, height: 80)
-                        .background(Color("AccentColor"))
-                        .clipShape(Circle())
-                    
-                    Button(action:{
-                        showingAlert = true
-                    }, label: {
-                        Image(systemName: "stop.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("AccentColor"))
-                            .padding(8)
-                    })
-                        .frame(width: 80, height: 80)
-                        .background(Color("AccentColor2"))
-                        .clipShape(Circle())
-                }.padding(30).alert("Do you want to end the run?", isPresented: $showingAlert) {
-                    Button("No", role: .cancel) { }
-                    Button("Yes", role: .none) {
-                        saveRun(duration: stopwatchManager.timeStr, pace: "\(stopwatchManager.avePace)", distance: "\(stopwatchManager.distance)", lineCoord: stopwatchManager.lineCoordinates)
+                    }
+                    VStack{
+                        Text("Avg Pace")
+                            .font(Font.custom("Rubik-Regular", size:12))
+                        Text("\(String(format: "%.2f", stopwatchManager.avePace))\"")
+                            .font(Font.custom("Sansita-BoldItalic", size:32))
+                    }
+                }.padding(10)
+            
+                if stopwatchManager.mode == .running{
+                    HStack(spacing: 55){
+                        Button(action:{self.stopwatchManager.pause()}, label: {
+                            Image("Logo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:70)
+                                .padding(.trailing, 8)
+                            Text("Lepus OFF")
+                                .font(Font.custom("Sansita-BoldItalic", size:32))
+                                .foregroundColor(Color.black)
+                        })
+                            .padding(20)
+                            .frame(width: 350, height: 80)
+                            .background(Color("AccentColor3"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(color: .gray, radius: 4, x: 5, y: 5)
+                    }.padding(30)
+                }
+                else{
+                    HStack(spacing: 55){
+                        Button(action:{self.stopwatchManager.start()}, label: {
+                            Image(systemName: "play.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(Color("AccentColor2"))
+                                .padding(8)
+                        })
+                            .frame(width: 80, height: 80)
+                            .background(Color("AccentColor"))
+                            .clipShape(Circle())
                         
-                        self.stopwatchManager.stop()
-                        self.presentationMode.wrappedValue.dismiss()
+                        Button(action:{
+                            showingAlert = true
+                        }, label: {
+                            Image(systemName: "stop.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(Color("AccentColor"))
+                                .padding(8)
+                        })
+                            .frame(width: 80, height: 80)
+                            .background(Color("AccentColor2"))
+                            .clipShape(Circle())
+                    }.padding(30).alert("Do you want to end the run?", isPresented: $showingAlert) {
+                        Button("No", role: .cancel) { }
+                        Button("Yes", role: .none) {
+                            generateSnapshot(width: 300, height: 300, lineCoord: stopwatchManager.lineCoordinates)
+                        }
                     }
                 }
             }
+            .background(Color("BackgroundColor"))
         }
-        .background(Color("BackgroundColor"))
     }
     
-    func saveRun(duration:String, pace:String, distance:String, lineCoord:[CLLocationCoordinate2D]){
+    func saveRun(duration:String, pace:String, distance:String, url:String){
         let user = Auth.auth().currentUser
         var ref: DatabaseReference!
         ref = Database.database().reference()
-        let myDict:[String: String] = ["Duration":duration, "Pace":pace, "Distance":distance, "userId":user!.uid]
+        let myDict:[String: String] = ["Duration":duration, "Pace":pace, "Distance":distance, "Url":url, "userId":user!.uid]
 
         ref.child("Runs").childByAutoId().setValue(myDict)
-        generateSnapshot(width: 300, height: 300, lineCoord: lineCoord)
+        self.stopwatchManager.stop()
+        self.presentationMode.wrappedValue.dismiss()
         
     }
     
     func generateSnapshot(width: CGFloat, height: CGFloat, lineCoord:[CLLocationCoordinate2D]) {
+        // The region the map should display.
+        let region = MKCoordinateRegion(
+            center: self.stopwatchManager.locationManager.location.coordinate,
+            span: self.span
+        )
 
-      // The region the map should display.
-      let region = MKCoordinateRegion(
-        center: self.stopwatchManager.locationManager.location.coordinate,
-        span: self.span
-      )
+        // Map options.
+        let mapOptions = MKMapSnapshotter.Options()
+        let polyLine = MKPolyline(coordinates: lineCoord, count: lineCoord.count)
 
-      // Map options.
-      let mapOptions = MKMapSnapshotter.Options()
-      let polyLine = MKPolyline(coordinates: lineCoord, count: lineCoord.count)
-    
 
-      mapOptions.region = region
-      mapOptions.size = CGSize(width: width, height: height)
-      mapOptions.showsBuildings = true
+        mapOptions.region = region
+        mapOptions.size = CGSize(width: width, height: height)
+        mapOptions.showsBuildings = true
 
-      // Create the snapshotter and run it.
-      let snapshotter = MKMapSnapshotter(options: mapOptions)
-      snapshotter.start { (snapshotOrNil, errorOrNil) in
-        if let error = errorOrNil {
-          print(error)
-          return
+        // Create the snapshotter and run it.
+        let snapshotter = MKMapSnapshotter(options: mapOptions)
+        
+        // Takes the screenshot of the map
+        snapshotter.start { (snapshotOrNil, errorOrNil) in
+            if let error = errorOrNil {
+              print(error)
+              return
+            }
+            if let snapshot = snapshotOrNil {
+                drawLineOnImage(snapshot: snapshot, lineCoord: lineCoord)
+            }
         }
-        if let snapshot = snapshotOrNil {
-          self.snapshotImage = snapshot.image
-            drawLineOnImage(snapshot: snapshot, lineCoord: lineCoord)
-        }
-      }
     }
     
     func drawLineOnImage(snapshot: MKMapSnapshotter.Snapshot, lineCoord:[CLLocationCoordinate2D]){
         let image = snapshot.image
-        let yourCoordinates = lineCoord
+        let lineCoordinates = lineCoord
+        
         // for Retina screen
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 300, height: 300), true, 0)
 
@@ -170,10 +169,10 @@ struct RunView: View {
         // We use addLine() and move() to draw the line, this should be easy to understand.
         // The diificult part is that they both take CGPoint as parameters, and it would be way too complex for us to calculate by ourselves
         // Thus we use snapshot.point() to save the pain.
-        context!.move(to: snapshot.point(for: yourCoordinates[0]))
-        for i in 0...yourCoordinates.count-1 {
-          context!.addLine(to: snapshot.point(for: yourCoordinates[i]))
-          context!.move(to: snapshot.point(for: yourCoordinates[i]))
+        context!.move(to: snapshot.point(for: lineCoordinates[0]))
+        for i in 0...lineCoordinates.count-1 {
+          context!.addLine(to: snapshot.point(for: lineCoordinates[i]))
+          context!.move(to: snapshot.point(for: lineCoordinates[i]))
         }
 
         // apply the stroke to the context
@@ -190,26 +189,27 @@ struct RunView: View {
     
     func upload(imagetoUpload: UIImage) {
         let storage = Storage.storage()
-        let storageRef = storage.reference().child("images")
+        let storageRef = storage.reference().child("\(UUID())")
     
         // Convert the image into JPEG and compress the quality to reduce its size
         let data = imagetoUpload.jpegData(compressionQuality: 0.2)
         // Change the content type to jpg. If you don't, it'll be saved as application/octet-stream type
-                let metadata = StorageMetadata()
-                metadata.contentType = "image/jpg"
-
-                // Upload the image
-                if let data = data {
-                    storageRef.putData(data, metadata: metadata) { (metadata, error) in
-                        if let error = error {
-                            print("Error while uploading file: ", error)
-                        }
-
-                        if let metadata = metadata {
-                            print("Metadata: ", metadata)
-                        }
-                    }
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        
+        // Upload the image
+        if let data = data {
+            storageRef.putData(data, metadata: metadata) { (metadata, error) in
+                if let error = error {
+                    print("Error while uploading file: ", error)
                 }
+            
+            // To get URL for display in run history
+            storageRef.downloadURL(completion: { (url: URL?, error: Error?) in
+                saveRun(duration: stopwatchManager.timeStr, pace: "\(stopwatchManager.avePace)", distance: "\(stopwatchManager.distance)", url: url!.absoluteString)
+                    })
+            }
+        }
     }
 }
 
