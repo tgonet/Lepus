e20115bc222544dce8e67675a660f8023669b9bf
Ming Zhe <tanmingzhe.99@gmail.com>
2022-01-27 02:39:43 +0800

    Bump

--- Lepus/NetworkManager.swift
+++ Lepus/NetworkManager.swift
@@ -25,7 +25,6 @@ class NetworkManager : ObservableObject{
                     print("No connection.")
                 }
             }
-            print(path.status)
         }
         monitor.start(queue: queue)
     }
